e6c145f7c8807451ecdafa501bbd31af4e08742a
Duane <duanerichard47@hotmail.com>
2023-01-27 21:56:38 -0500

    TC coach says something wrong with test

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Add your code here
+
+
+function submitData(name, email){
+
+fetch('http://localhost:3000/users',{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+            name,
+            email
+        })
+})
+.then( function ( response ) {
+        return response.json()
+        } )
+.then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+        } )
+.catch(error=> {
+    document.body.innerHTML =  error.message
+    })
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function submitData( user_name, user_email){
+//     return fetch("http://localhost:3000/users", { 
+//         method : "POST",
+//         headers : {
+//             "Content-Type": "application/json",
+//             "Accept" : "application/json",
+//         },
+//         body : JSON.stringify({
+//             name, //: user_name,
+//             email  //: user_email,
+//         })
+//     })
+//     // .then(response=>response.json())
+//     .then( function ( response ) {
+//         return response.json()
+//         } )
+//     .then( function ( object ) {
+//         document.body.innerHTML = object[ "id" ]
+//         } )
+
+//     // return fetch
+// }
